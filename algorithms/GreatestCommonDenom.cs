76bd49cac8605f8e495a2469d5058b395f0c53f0
HankBedford <Zachary.Lynch1337@gmail.com>
2019-01-16 13:48:04 -0800

    Wrote a program that got greatest common denominator

---
+++ algorithms/GreatestCommonDenom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace algorithms
+{
+    class GreatestCommonDenom
+    {
+        public int common (int a, int b) {
+            while (b != 0) {
+                int temp = a;
+                a = b;
+                b = temp % b;
+            }
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            var p = new GreatestCommonDenom();
+            int result = p.common(20, 8);
+            Console.WriteLine(result);
+        }
+    }
+}
