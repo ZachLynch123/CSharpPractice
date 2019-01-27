0614ee410c90652e5a5845cfd992a0c539a510d8
Zach <Zachary.Lynch1337@gmail.com>
2019-01-27 12:16:58 -0800

    Powerand factoral functions written using recursion

--- recursion/recursion.py
+++ recursion/recursion.py
@@ -10,3 +10,26 @@ def countdown(x):
         return
 
 countdown(5)
+
+
+## building factorial and power functions
+
+def power(num, pwr):
+    if pwr == 0:
+        return 1
+    else:
+        return num * power(num, pwr - 1)
+
+
+def factorial(num):
+    if num == 0:
+        return 1
+
+    else:
+        return num * factorial(num - 1)
+
+
+print("{} to the power of {} is {}".format(5, 3, power(5, 3)))
+print("{} to the power of {} is {}".format(5, 3, power(7, 5)))
+print("{}! is {}".format(4, factorial(4)))
+print("{}! is {}".format(2, factorial(2)))
